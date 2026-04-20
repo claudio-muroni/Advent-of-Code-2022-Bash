95f41dda9593ff101a9223a11085ea68425e1f9b
claudio-muroni <muroni.claudio@gmail.com>
2026-04-21 00:18:16 +0200

    WIP Day 03 part 1

--- Day03_1.sh
+++ Day03_1.sh
@@ -2,7 +2,27 @@
 
 file="Test_Input_Day03_1.txt"
 
+alphabet="_abcdefghikjlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+tot=0
+
 while read -r line; do
-	echo $line
+
+	len=$((${#line}/2))
+	h1=${line:0:$len}
+	h2=${line:$len}
+
+	for (( i=0; i<${#h1}; i++ )); do
+		
+		c=${h1:$i:1}
+		if [[ $h2 =~ $c ]]; then
+			break
+		fi
+
+	done
+
+	pre=${alphabet%%$c*}
+	(( tot+=${#pre} ))
 
 done < $file
+
+echo "Day 03 part 1 solution: $tot"
