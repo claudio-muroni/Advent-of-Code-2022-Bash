e93e7b28c0df4799cefaff39381177ae71917647
claudio-muroni <muroni.claudio@gmail.com>
2026-04-22 22:52:40 +0200

    WIP Day 03 part 2

---
+++ Day03_2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+file="Test_Input_Day03_1.txt"
+
+alphabet="_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+tot=0
+
+mapfile lines < $file
+
+i=0
+while [ $i -le ${#lines[@]} ]; do
+	line=${lines[i]}
+
+	for (( j=0; j<${#line}; j++ )); do
+
+		c=${line:$j:1}
+		if [[ ${line[j+1]} =~ $c && ${line[j+2]} =~ $c ]]; then
+			echo $c
+			break
+		fi
+	done
+
+	(( i+=2 ))
+done
+
+
+echo "Day 03 part 2 solution: $tot"
