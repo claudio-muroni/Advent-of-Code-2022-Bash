3031b66134cdaba85d99362e7329f59dd3c507d3
claudio-muroni <muroni.claudio@gmail.com>
2026-04-24 22:56:08 +0200

    Solved Day 03 part 2

--- Day03_2.sh
+++ Day03_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file="Test_Input_Day03_1.txt"
+file="Input_Day03_1.txt"
 
 alphabet="_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 tot=0
@@ -8,21 +8,24 @@ tot=0
 mapfile lines < $file
 
 i=0
-while [ $i -le ${#lines[@]} ]; do
+while [ $i -lt ${#lines[@]} ]; do
+	
 	line=${lines[i]}
-	echo $line
-	echo ${lines[j+1]}
-	echo ${lines[j+2]}
+
 	for (( j=0; j<${#line}; j++ )); do
 
 		c=${line:$j:1}
-		if [[ ${lines[j+1]} =~ $c && ${lines[j+2]} =~ $c ]]; then
-			echo $c
+		if [[ ${lines[i+1]} =~ $c && ${lines[i+2]} =~ $c ]]; then
 			break
 		fi
+
 	done
 
-	(( i+=2 ))
+	pre=${alphabet%%$c*}
+        (( tot+=${#pre} ))
+
+
+	(( i+=3 ))
 done
 
 
